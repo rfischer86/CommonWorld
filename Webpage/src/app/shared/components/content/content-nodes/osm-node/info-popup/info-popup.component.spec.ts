09cf76194357bf636804c6224350e09d5050068f
Rico <fischherrico@gmail.com>
2021-02-22 18:09:34 +0100

    During update

--- Webpage/src/app/shared/components/content/content-nodes/osm-node/info-popup/info-popup.component.spec.ts
+++ Webpage/src/app/shared/components/content/content-nodes/osm-node/info-popup/info-popup.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InfoPopupComponent } from './info-popup.component';
 
@@ -6,7 +6,7 @@ describe('InfoPopupComponent', () => {
   let component: InfoPopupComponent;
   let fixture: ComponentFixture<InfoPopupComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InfoPopupComponent ]
     })
--- Webpage/src/app/shared/components/content/content-nodes/osm-node/osm.component.spec.ts
+++ Webpage/src/app/shared/components/content/content-nodes/osm-node/osm.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OsmComponent } from './osm.component';
 
@@ -6,7 +6,7 @@ describe('OsmComponent', () => {
   let component: OsmComponent;
   let fixture: ComponentFixture<OsmComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OsmComponent ]
     })
